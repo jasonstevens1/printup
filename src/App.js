cf4bfb434d185caf172b5cd0e5a0c104997196c1
Jason Stevens <jason.stevens@shaw.ca>
2018-11-19 11:12:52 -0700

    Base build of empty fabric canvas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import React, { Component } from "react";
+import FabricCanvas from "./components/FabricCanvas";
+import { fabric } from "fabric";
 import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      activeProperty: null
+    };
+  }
+
+  addToCanvas = (imgElement, property_type, z_Index) => {
+    var imgInstance = new fabric.Image(imgElement, {
+      width: 1024,
+      height: 768,
+      the_type: property_type,
+      zIndex: z_Index
+    });
+
+    this.setState({ activeProperty: imgInstance });
+  };
+
   render() {
     return (
       <div className="App">
-        <h1>PrintUp</h1>
+        <div className="main">
+          <div className="row">
+            <div className="col-md-12">
+              <FabricCanvas activeProperty={this.state.activeProperty} />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/components/FabricCanvas.js
@@ -0,0 +1,75 @@
+// FabricCanvas Component by Ali Zohaib
+// https://github.com/alizohaib/avatar-maker
+
+import React from "react";
+import { fabric } from "fabric";
+
+class FabricCanvas extends React.Component {
+  componentDidMount() {
+    // Make a New Canvas
+    this.the_canvas = new fabric.StaticCanvas("main-canvas", {
+      preserveObjectStacking: true,
+      width: 1024,
+      height: 768
+    });
+  }
+
+  componentWillReceiveProps = newprops => {
+    // If Updated Item is not the same as the old one
+    // 		=> Update the canvas with newer item
+    if (newprops.activeProperty !== this.props.activeProperty) {
+      this.updateCanvasforImage(
+        this.props.activeProperty,
+        newprops.activeProperty
+      );
+    }
+  };
+
+  updateCanvasforImage = (prev, next) => {
+    if (next) {
+      let to_remove;
+      // Find the same kind of element
+      this.the_canvas.forEachObject(object => {
+        if (object.the_type === next.the_type) {
+          to_remove = object;
+        }
+      });
+
+      this.the_canvas.remove(to_remove);
+
+      if (next.the_type === "bg") {
+        this.the_canvas.setBackgroundImage(next);
+        this.the_canvas.renderAll();
+        return;
+      }
+
+      this.the_canvas.add(next);
+      this.the_canvas.moveTo(next, next.zIndex);
+    }
+  };
+
+  saveToCanvas = () => {
+    let link = document.createElement("a");
+    link.href = this.the_canvas.toDataURL({ format: "png" });
+    link.download = "avatar.png";
+    link.click();
+  };
+
+  render() {
+    return (
+      <div className="main-canvas-container">
+        <canvas id="main-canvas" />
+
+        <button
+          type="button"
+          className="btn btn-success"
+          onClick={this.saveToCanvas}
+        >
+          Download Image
+        </button>
+      </div>
+    );
+  }
+}
+
+export default FabricCanvas;
